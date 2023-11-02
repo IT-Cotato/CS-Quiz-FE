575aebbf4b44bef70bbec608b07c9d8ec5f4f2c7
MinJaeSon <mjsonsj@gmail.com>
2023-11-02 23:41:39 +0900

    [style] 색상 변수화

---
+++ src/theme/Theme.jsx
@@ -0,0 +1,15 @@
+export const Theme = {
+  colors: {
+    white: '#FFFFFF',
+    skyBlue: '#D2E0FB',
+    yellow: '#F9F3CC',
+    lightGreen: '#D7E5CA',
+    darkBlue: '#8EACCD',
+    lightGrey: '#CECCCC',
+    grey: '#5E5E5E',
+    darkGrey: '#7B7B7B',
+    black: '#000000',
+    red: '#EB5353',
+    green: '#85C88A',
+  },
+};
