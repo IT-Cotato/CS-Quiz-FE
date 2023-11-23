747fa46a627c93deb9b19c6e7dca90d49c7678ee
MinJaeSon <mjsonsj@gmail.com>
2023-11-23 18:48:37 +0900

    [feat] 초기 라우팅 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,35 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Header from '@pages/Home/Header';
+import Home from './pages/Home/Home';
+import Projects from './pages/Projects/Projects';
+import Team from './pages/Team/Team';
+import CSHome from './pages/CS/CSHome';
+import Login from './pages/Login/Login';
+import SessionHome from './pages/Session/SessionHome';
+import Recruitment from './pages/JoinUs/Recruitment';
+import MyPage from './pages/MyPage/MyPage';
 
 function App() {
-  return <div>Hello World!</div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/team" element={<Team />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/cs" element={<CSHome />} />
+          <Route path="/session" element={<SessionHome />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/joinus" element={<Recruitment />} />
+          <Route path="/mypage" element={<MyPage />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
