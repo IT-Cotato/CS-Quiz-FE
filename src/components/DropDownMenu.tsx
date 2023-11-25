260359b64c6c3fcfbb97fc0d3b55727d310c2e94
MinJaeSon <mjsonsj@gmail.com>
2023-11-25 15:59:24 +0900

    [feat] 헤더 구현

---
+++ src/components/DropDownMenu.tsx
@@ -0,0 +1,119 @@
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import arrowDown from '@assets/arrow_down.svg';
+import arrowUp from '@assets/arrow_up.svg';
+import { Link } from 'react-router-dom';
+
+const DropDownMenu = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <>
+      {isOpen ? (
+        <OpenedMenu>
+          <MenuWrapper>
+            <p>Members Only</p>
+            <img onClick={() => setIsOpen(!isOpen)} src={isOpen ? arrowUp : arrowDown} />
+          </MenuWrapper>
+          <ul>
+            <li>
+              <MenuItem to="/session">세션기록</MenuItem>
+            </li>
+            <li>
+              <MenuItem to="/cs">CS 문제풀이</MenuItem>
+            </li>
+            <li>
+              <MenuItem to="/mypage">마이페이지</MenuItem>
+            </li>
+          </ul>
+        </OpenedMenu>
+      ) : (
+        <ClosedMenu>
+          <MenuWrapper>
+            Members Only
+            <img onClick={() => setIsOpen(!isOpen)} src={isOpen ? arrowUp : arrowDown} />
+          </MenuWrapper>
+        </ClosedMenu>
+      )}
+    </>
+  );
+};
+
+export default DropDownMenu;
+
+const OpenedMenu = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: flex-start;
+  margin-top: 116px;
+  position: relative;
+  color: #93c6fe;
+  font-size: 1.2rem;
+  font-weight: 500;
+  margin-left: 36px;
+  width: 220px;
+  height: 168px;
+  border: 3px solid #93c6fe;
+  border-radius: 50px;
+  background-color: #fff;
+  img {
+    width: 24px;
+    margin-left: 10px;
+  }
+  ul {
+    margin-top: 40px;
+    margin-right: 40px;
+    position: absolute;
+    list-style: none;
+    top: 20px;
+    transform: translateY(0);
+    transition:
+      opacity 0.4s ease,
+      transform 0.4s ease,
+      visibility 0.4s;
+  }
+  li {
+    font-family: NanumSquareRound;
+    margin-bottom: 8px;
+  }
+`;
+
+const MenuWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  font-weight: 500;
+  p {
+    margin-top: 13px;
+  }
+`;
+
+const MenuItem = styled(Link)`
+  color: #93c6fe;
+  font-size: 1.2rem;
+  font-weight: 500;
+  letter-spacing: 4px;
+  text-decoration: none;
+`;
+
+const ClosedMenu = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  position: relative;
+  color: #93c6fe;
+  font-size: 1.2rem;
+  font-weight: 500;
+  margin-left: 36px;
+  width: 220px;
+  height: 52px;
+  border: 3px solid #93c6fe;
+  border-radius: 50px;
+  img {
+    width: 24px;
+    margin-left: 10px;
+  }
+`;
---
+++ src/components/Header.tsx
@@ -0,0 +1,92 @@
+import React from 'react';
+import styled from 'styled-components';
+import logo from '@assets/logo.svg';
+import login from '@assets/login_icon.svg';
+import joinus from '@assets/joinus_icon.svg';
+import { Link } from 'react-router-dom';
+import DropDownMenu from './DropDownMenu';
+
+const Header = () => {
+  return (
+    <Wrapper>
+      <Logo src={logo} onClick={() => window.location.replace('/')} style={{ cursor: 'pointer' }} />
+      <MenuSection>
+        <MenuItem to="/">홈</MenuItem>
+        <MenuItem to="/projects">프로젝트</MenuItem>
+        <MenuItem to="/team">팀 멤버</MenuItem>
+      </MenuSection>
+      <LoginSection>
+        <LoginWrapper>
+          <img src={login} />
+          <LoginItem to="/login">로그인</LoginItem>
+        </LoginWrapper>
+        <LoginWrapper>
+          <img src={joinus} />
+          <LoginItem to="/joinus">Join Us</LoginItem>
+        </LoginWrapper>
+      </LoginSection>
+      <DropDownMenu />
+    </Wrapper>
+  );
+};
+
+export default Header;
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 80px;
+  box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
+`;
+
+const Logo = styled.img`
+  width: 152px;
+`;
+
+const MenuSection = styled.div`
+  display: flex;
+  flex-direction: row;
+  margin-left: 132px;
+`;
+
+const MenuItem = styled(Link)`
+  color: rgba(0, 0, 0, 0.6);
+  font-size: 1.2rem;
+  font-weight: 400;
+  text-decoration: none;
+  margin-right: 80px;
+  font-family: NanumSquareRound;
+  &:hover {
+    color: #000;
+  }
+`;
+
+const LoginSection = styled.div`
+  display: flex;
+  flex-direction: row;
+  margin-left: 220px;
+`;
+
+const LoginWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  margin-right: 40px;
+  width: 100px;
+  img {
+    width: 20px;
+    height: 20px;
+  }
+`;
+
+const LoginItem = styled(Link)`
+  color: var(--pointcolor-4, #259c2e);
+  font-size: 1.2rem;
+  font-weight: 400;
+  text-decoration: none;
+  margin-left: 8px;
+  font-family: NanumSquareRound;
+`;
