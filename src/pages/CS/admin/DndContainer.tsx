bd2136d85b82ad861eeddd1ae34314c9d93fc0d8
Ea-st-ring <95581482+Ea-st-ring@users.noreply.github.com>
2024-01-12 01:53:34 +0900

    [feat] 드래그 앤 드랍 컴포넌트 분리

---
+++ src/pages/CS/admin/DndContainer.tsx
@@ -0,0 +1,103 @@
+import React, { useCallback } from 'react';
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
+import styled from 'styled-components';
+import { ChoiceProps, ShortProps } from '@/typing/db';
+
+type Item = {
+  isselected?: string;
+};
+
+type QuizType = ChoiceProps | ShortProps;
+/**
+ * 드래그 앤 드랍 컨테이너 컴포넌트
+ * @param item : 문제를 담은 배열
+ * @param setItem : 문제를 담은 배열을 변경하는 함수
+ * @param setSelected : 선택된 문제의 id를 변경하는 함수
+ * @param selected : 선택된 문제의 id
+ * @returns
+ */
+const DndContainer = (
+  item: QuizType[],
+  setItem: React.Dispatch<React.SetStateAction<QuizType[]>>,
+  setSelected: React.Dispatch<React.SetStateAction<number>>,
+  selected: number,
+) => {
+  const handleDragEnd = useCallback(
+    (result: any) => {
+      if (!result.destination) {
+        return;
+      }
+
+      const items = Array.from(item);
+      const [reorderedItem] = items.splice(result.source.index, 1);
+      items.splice(result.destination.index, 0, reorderedItem);
+      // id 재정렬
+      const itemsWithNewIds = items.map((item, index) => ({
+        ...item,
+        quiz_id: index + 1,
+      }));
+      setItem(itemsWithNewIds);
+      setSelected(result.destination.index + 1);
+    },
+    [item],
+  );
+
+  return (
+    <DragDropContext onDragEnd={handleDragEnd}>
+      <Droppable droppableId="droppable">
+        {(provided) => (
+          <List {...provided.droppableProps} ref={provided.innerRef}>
+            {item.map((item, index) => (
+              <Draggable
+                key={`draggable-${item.quiz_id}`}
+                draggableId={`draggable-${item.quiz_id}`}
+                index={index}
+              >
+                {(provided) => (
+                  <div
+                    ref={provided.innerRef}
+                    {...provided.draggableProps}
+                    {...provided.dragHandleProps}
+                  >
+                    <Item
+                      key={item.quiz_id}
+                      isselected={selected === item.quiz_id ? 'true' : 'false'}
+                      onClick={() => {
+                        setSelected(item.quiz_id);
+                      }}
+                    >
+                      {item.quiz_id}
+                    </Item>
+                  </div>
+                )}
+              </Draggable>
+            ))}
+            {provided.placeholder}
+          </List>
+        )}
+      </Droppable>
+    </DragDropContext>
+  );
+};
+
+const List = styled.div`
+  flex-direction: column;
+  height: fit-content !important;
+`;
+
+const Item = styled.div<Item>`
+  width: 200px;
+  height: 48px;
+  border-radius: 5px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+  border: none;
+  background: ${(props: any) => (props.isselected === 'true' ? '#C4D7FF' : '#E4ECFD')};
+  box-shadow: 0px 4px 5px 0px rgba(0, 0, 0, 0.15);
+  margin: 16px 0;
+  transition: 0.2s;
+`;
+
+export default DndContainer;
