b66b44bf78cefb556a53e33919ff61b32b319f72
Ea-st-ring <95581482+Ea-st-ring@users.noreply.github.com>
2024-02-15 01:43:42 +0900

    [feat] 애니메이션 변경

--- src/pages/Home/Ellipse.tsx
+++ src/pages/Home/Ellipse.tsx
@@ -5,15 +5,24 @@ type EllipseProps = {
   top: string;
   left: string;
   time: number;
+  width?: string;
+  height?: string;
+  delay?: number;
+  src?: string;
 };
 
-const Ellipse = ({ top, left, time }: EllipseProps) => {
+const Ellipse = ({ top, left, time, width, height, src }: EllipseProps) => {
   return (
     <Image
-      src="https://velog.velcdn.com/images/ea_st_ring/post/ed87f665-b5e7-46fb-b9ec-224d4343ce37/image.svg"
+      src={
+        src ||
+        'https://velog.velcdn.com/images/ea_st_ring/post/79c49a08-8315-4b9c-9914-f07fba77628c/image.svg'
+      }
       top={top}
       left={left}
       time={time}
+      width={width}
+      height={height}
     />
   );
 };
@@ -22,14 +31,20 @@ const Image = styled.img<EllipseProps>`
   position: absolute;
   top: ${(props) => props.top};
   left: ${(props) => props.left};
-  animation: move ${(props) => props.time}s linear infinite;
+  width: ${(props) => props.width};
+  height: ${(props) => props.height};
+  animation: float ${(props) => props.time}s linear infinite;
+  animation-delay: ${(props) => props.delay}s;
   z-index: 3;
-  @keyframes move {
+  @keyframes float {
     0% {
       transform: translateY(0);
     }
+    50% {
+      transform: translateY(3vh);
+    }
     100% {
-      transform: translate(-40vw, -130vh);
+      transform: translateY(0);
     }
   }
 `;
