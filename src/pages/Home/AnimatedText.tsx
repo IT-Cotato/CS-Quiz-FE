8650de31e75b12f448df3f31c16f0e3b754c2745
Ea-st-ring <95581482+Ea-st-ring@users.noreply.github.com>
2024-02-19 04:14:58 +0900

    [feat] 로직 변경
    제발

--- src/pages/Home/AnimatedText.tsx
+++ src/pages/Home/AnimatedText.tsx
@@ -36,7 +36,9 @@ const AnimatedText = () => {
     'r',
     '!',
   ]);
-  const spaces = [0, 0, -105, -94, -276, -277];
+  const [mobile, setMobile] = useState(window.innerWidth < 768 ? true : false);
+  const [spaces, setSpaces] = useState<number[]>([]);
+
   const checkUpper = (char: string) => {
     return char === char.toUpperCase() && char.match(/[a-zA-Z]/);
   };
@@ -44,15 +46,43 @@ const AnimatedText = () => {
   const animDelay = 3; // 최소 2
   const line1Ref = React.useRef<HTMLDivElement>(null);
   const line2Ref = React.useRef<HTMLDivElement>(null);
+  const charORef = React.useRef<HTMLSpanElement>(null);
   let topValue = 0;
+  let leftValue = 0;
+  let widthValue = 0;
 
   if (line1Ref.current && line2Ref.current) {
     topValue = (line1Ref as any).current?.offsetTop - (line2Ref as any).current?.offsetTop;
-    console.log(topValue);
   }
+  if (charORef.current) {
+    leftValue = charORef.current.offsetLeft;
+    widthValue = charORef.current.offsetWidth;
+  }
+
+  useEffect(() => {
+    if (charORef.current) {
+      setSpaces([
+        0,
+        0,
+        -leftValue - widthValue * 1.4,
+        -leftValue - widthValue * 1.3,
+        -leftValue - widthValue - 205,
+        -leftValue - widthValue - 205,
+      ]);
+    }
+  }, [charORef.current]);
+
+  window.addEventListener('resize', () => {
+    if (window.innerWidth < 768) {
+      setMobile(true);
+    } else {
+      setMobile(false);
+    }
+  });
 
   return (
     <Box $anim_delay={animDelay}>
+      <h5 className="subtitle">IT 연합동아리 코테이토</h5>
       <Line1 ref={line1Ref}>
         {line1.map((char, idx) =>
           checkUpper(char) ? (
@@ -60,8 +90,8 @@ const AnimatedText = () => {
               key={idx}
               $anim_delay={animDelay}
               top={0}
-              left={spaces.shift() || 0}
-              className="notranslate"
+              left={char === 'C' ? spaces[0] : char === 'O' ? spaces[1] : spaces[2]}
+              ref={idx === 1 ? charORef : null}
             >
               {char}
             </UpperChar>
@@ -79,9 +109,8 @@ const AnimatedText = () => {
             <UpperChar
               key={idx}
               $anim_delay={animDelay}
-              top={topValue || -118}
-              left={spaces.shift() || 0}
-              className="notranslate"
+              top={topValue}
+              left={char === 'A' ? spaces[3] : char === 'T' ? spaces[4] : spaces[5]}
             >
               {char}
             </UpperChar>
@@ -92,6 +121,9 @@ const AnimatedText = () => {
           ),
         )}
       </Line2>
+
+      {mobile && <MobileTitle>COTATO</MobileTitle>}
+
       <div className="typing-text">Who&apos;s the next potato?</div>
       <div className="join-button">
         <p
@@ -110,10 +142,18 @@ const AnimatedText = () => {
 const Box = styled.div<{ $anim_delay: number }>`
   display: flex;
   flex-direction: column;
-  transition: 2s;
-  height: fit-content;
+  justify-content: flex-start;
+  height: 500px;
   margin-top: 130px;
   cursor: default;
+  .subtitle {
+    font-size: 16px;
+    color: #fff;
+    text-align: start;
+    margin: 0 0 200px 6px;
+    opacity: 0;
+    animation: fadeIn 2s ${(props) => props.$anim_delay + 1}s forwards;
+  }
   .animated-char {
     /* opacity: 0; */
     animation: fadeOut 1s ${(props) => props.$anim_delay - 1}s ease-in forwards;
@@ -131,12 +171,21 @@ const Box = styled.div<{ $anim_delay: number }>`
     to {
       opacity: 0;
       visibility: hidden;
+      /* display: none; */
+    }
+  }
+  @keyframes fadeIn {
+    from {
+      opacity: 0;
+    }
+    to {
+      opacity: 1;
     }
   }
   .typing-text {
     width: 18.5ch;
     animation:
-      fadeIn 4s ${(props) => props.$anim_delay}s forwards,
+      fadeInAndGoUp 4s ${(props) => props.$anim_delay}s forwards,
       typing 2s ${(props) => props.$anim_delay}s steps(24),
       blink 0.5s ${(props) => props.$anim_delay}s step-end infinite alternate;
     white-space: nowrap;
@@ -145,7 +194,7 @@ const Box = styled.div<{ $anim_delay: number }>`
     font-size: 1.8em;
     opacity: 0;
   }
-  @keyframes fadeIn {
+  @keyframes fadeInAndGoUp {
     from {
       opacity: 0;
     }
@@ -175,6 +224,7 @@ const Box = styled.div<{ $anim_delay: number }>`
     align-items: center;
     animation: leftToRight 3s ${(props) => props.$anim_delay + 2}s forwards;
     opacity: 0;
+    border-radius: 25px;
     cursor: pointer;
     p {
       color: #fff;
@@ -195,7 +245,6 @@ const Box = styled.div<{ $anim_delay: number }>`
     &:hover {
       width: 200px;
       background: linear-gradient(180deg, #6e6cfe 0%, rgba(189, 219, 255, 0.69) 100%);
-      border-radius: 25px;
 
       img {
         transform: translateX(-5px);
@@ -212,32 +261,74 @@ const Box = styled.div<{ $anim_delay: number }>`
       transform: translateY(-20px) translateX(20px);
     }
   }
+  @media screen and (max-width: 768px) {
+    margin-top: 120px;
+    .subtitle {
+      font-size: 14px;
+      animation: fadeIn 0s 0s forwards;
+      opacity: 1;
+    }
+    .typing-text {
+      font-size: 1.5em;
+      animation:
+        fadeInAndGoUp 0s 0s forwards,
+        typing 0s 0s steps(24),
+        blink 0.5s 0s step-end infinite alternate;
+    }
+    .join-button {
+      p {
+        font-size: 20px;
+      }
+      animation: leftToRight 0s 0s forwards;
+    }
+    .animated-char {
+      animation: fadeOut 0s 0s ease-in forwards;
+    }
+  }
 `;
 const Line1 = styled.div`
   display: flex;
   flex-direction: row;
   height: 100px;
+  position: absolute;
+  top: 285px;
 `;
 
 const Line2 = styled.div<{ $anim_delay: number }>`
   display: flex;
   flex-direction: row;
   height: 100px;
+  position: absolute;
+  top: 385px;
   animation: moveRight 3s ${(props) => props.$anim_delay - 2}s forwards;
   @keyframes moveRight {
     0% {
-      margin-left: 0px;
       transform: translateX(0px);
     }
     100% {
       transform: translateX(240px);
     }
   }
+  @media screen and (max-width: 768px) {
+    opacity: 0;
+    animation: moveRight 0s 0s forwards;
+  }
 `;
 
+const MobileTitle = styled.h2`
+  position: absolute;
+  font-size: 50px;
+  top: 300px;
+  margin: 0;
+  font-family: Pretendard;
+`;
 const UpperChar = styled.span<{ top: number; left: number; $anim_delay: number }>`
   animation: ${(props) => delayPosition(props.top, props.left)} 3s
     ${(props) => props.$anim_delay + 0.5}s forwards;
+  @media screen and (max-width: 768px) {
+    opacity: 0;
+    /* animation: ${(props) => delayPosition(props.top, props.left)} 0s 0s forwards; */
+  }
 `;
 
 const delayPosition = (top: number, left: number) => keyframes`
