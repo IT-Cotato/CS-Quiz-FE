0569192090382dd6cb63cd34196f8583e18637ff
Ea-st-ring <95581482+Ea-st-ring@users.noreply.github.com>
2024-02-18 17:37:09 +0900

    [feat] 홈 전용 헤더 퍼블리싱

---
+++ src/components/HomeHeader.tsx
@@ -0,0 +1,78 @@
+import React from 'react';
+import styled from 'styled-components';
+import { useNavigate } from 'react-router-dom';
+import HomeDropDownMenu from './HomeDropDownMenu';
+
+const HomeHeader = () => {
+  const navigate = useNavigate();
+
+  return (
+    <Wrapper>
+      <span>COTATO</span>
+      <ul>
+        <li>홈</li>
+        <li>프로젝트</li>
+        <li>팀 멤버</li>
+        <li
+          onClick={() => {
+            navigate('/login');
+          }}
+        >
+          로그인
+        </li>
+        <li>Join us</li>
+        <HomeDropDownMenu />
+      </ul>
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.div`
+  width: 100%;
+  padding: 30px 50px;
+  display: flex;
+  align-items: flex-start;
+  justify-content: space-between;
+  background: transparent;
+  z-index: 10;
+  position: fixed;
+  span {
+    z-index: 10;
+    color: #fff;
+    text-align: center;
+    font-family: Pretendard;
+    font-size: 24px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: normal;
+    letter-spacing: 1.28px;
+  }
+  ul {
+    display: flex;
+    flex-direction: row;
+    align-items: flex-start;
+    list-style: none;
+    margin: 0;
+    z-index: 10;
+    li {
+      color: #fff;
+      font-size: 16px;
+      font-weight: 400;
+      margin-right: 40px;
+      z-index: 10;
+      font-family: NanumSquareRound;
+      &:hover {
+        color: #fff;
+        font-weight: 600;
+      }
+    }
+    li + li {
+      margin-right: 64px;
+    }
+    li:first-child {
+      margin-right: 80px;
+    }
+  }
+`;
+
+export default HomeHeader;
