b00a684db63da8f38d8abe770dc5d854d1842386
WONYOUNG-HC <whdnjsdud551@gmail.com>
2024-02-20 00:59:40 +0900

    [chore] api 인스턴스
    헤더 토큰 포함 및 credentials: true 설정

--- src/api/api.ts
+++ src/api/api.ts
@@ -2,6 +2,10 @@ import axios from 'axios';
 
 const api = axios.create({
   baseURL: process.env.REACT_APP_BASE_URL,
+  headers: {
+    Authorization: `Bearer ${localStorage.getItem('token')}`,
+  },
+  withCredentials: true,
 });
 
 export default api;
