50c67e948252f280ed7b8bdc4717d1e5a585838e
WONYOUNG-HC <whdnjsdud551@gmail.com>
2024-02-20 06:02:17 +0900

    [feat] 기수 추가 모달 퍼블리싱

--- src/pages/MyPage/setting/GenerationModal.tsx
+++ src/pages/MyPage/setting/GenerationModal.tsx
@@ -4,11 +4,7 @@ import { styled } from 'styled-components';
 import { ReactComponent as CloseIcon } from '@assets/close_icon.svg';
 import TextBox from '@components/TextBox';
 import { ReactComponent as CalendarIcon } from '@assets/calendar_icon.svg';
-import { format } from 'date-fns';
-import { ko } from 'date-fns/locale';
-import { DayPicker } from 'react-day-picker';
-import 'react-day-picker/dist/style.css';
-import dayjs from 'dayjs';
+import GenerationDayPicker from '@pages/MyPage/setting/GenerationDayPicker';
 
 interface Props {
   isOpen: boolean;
@@ -18,38 +14,32 @@ interface Props {
 const GenerationModal = ({ isOpen, onCloseModal }: Props) => {
   const [generation, setGeneration] = useState('');
   const [isCalendarOpen, setIsCalendarOpen] = useState(false);
-  const [days, setDays] = useState<Date[]>();
   const [period, setPeriod] = useState('');
   const [sessionCount, setSessionCount] = useState('');
 
   const onChageGeneration = useCallback(
     (e: ChangeEvent<HTMLTextAreaElement>) => {
-      const num = parseInt(e.target.value);
-
-      if (num) {
-        setGeneration(`${num}기`);
-      } else {
-        setGeneration(e.target.value);
-      }
+      setGeneration(e.target.value);
     },
     [generation],
   );
 
   const onChageSessionCount = useCallback(
     (e: ChangeEvent<HTMLTextAreaElement>) => {
-      const num = parseInt(e.target.value);
-
-      if (num) {
-        setSessionCount(`${num}개`);
-      } else {
-        setSessionCount(e.target.value);
-      }
+      setSessionCount(e.target.value);
     },
     [sessionCount],
   );
 
+  const cleanInputState = useCallback(() => {
+    setGeneration('');
+    // setDays(undefined);
+    setPeriod('');
+    setSessionCount('');
+  }, []);
+
   return (
-    <ReactModal isOpen={isOpen} style={modalStyle}>
+    <ReactModal isOpen={isOpen} style={modalStyle} onAfterClose={cleanInputState}>
       <ModalWrapper>
         <ModalCloseButton>
           <CloseIcon width="36" height="36" fill="#BBBBBB" onClick={() => onCloseModal(false)} />
@@ -87,9 +77,7 @@ const GenerationModal = ({ isOpen, onCloseModal }: Props) => {
             <p>추가하기</p>
           </UploadButton>
         </ButtonContainer>
-        {isCalendarOpen && (
-          <StyledDayPicker locale={ko} mode="multiple" max={2} selected={days} onSelect={setDays} />
-        )}
+        {/* <GenerationDayPicker isOpen={isCalendarOpen} setIsOpen={setIsCalendarOpen} /> */}
       </ModalWrapper>
     </ReactModal>
   );
@@ -165,37 +153,6 @@ const CalendarButton = styled.button`
   border: none;
   border-radius: 12px;
   cursor: pointer;
-`;
-
-const StyledDayPicker = styled(DayPicker)`
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  background: #fff;
-  border: 1px solid #b2b2b2;
-  border-radius: 12px;
-
-  .rdp-months {
-    margin: 12px;
-
-    .rdp-head {
-      font-family: NanumSquareRound;
-      font-size: 20px;
-    }
-
-    .rdp-tbody {
-      font-family: NanumSquareRound;
-      font-weight: 300;
-
-      .rdp-day_today {
-        font-weight: 300;
-      }
-    }
-  }
 `;
 
 const Button = styled.button`
