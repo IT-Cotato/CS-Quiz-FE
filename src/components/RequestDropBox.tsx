52b8b71704ff777968adb472981a899e1530ad79
WONYOUNG-HC <whdnjsdud551@gmail.com>
2024-02-21 17:15:41 +0900

    [feat] 신입 요청, 거절 페이지 구현

--- src/components/RequestDropBox.tsx
+++ src/components/RequestDropBox.tsx
@@ -1,37 +1,100 @@
+import React, { useCallback, useEffect, useRef, useState } from 'react';
 import { IGeneration } from '@/typing/db';
 import fetcher from '@utils/fetcher';
-import React, { useEffect, useRef, useState } from 'react';
 import { styled } from 'styled-components';
+import { ReactComponent as ArrowDown } from '@assets/arrow_dwon_thin.svg';
+import { ReactComponent as ArrowUp } from '@assets/arrow_up_thin.svg';
 import useSWR from 'swr';
 
 interface Props {
   mode: string;
-  selectedValue?: string;
   width: string;
-  onChangeSelect?: () => void;
+  selectedGeneration?: IGeneration;
+  selectedPosition?: string;
+  onChangeGeneration?: (selectedGeneration: IGeneration) => void;
+  onChangePosition?: (selectedPosition: string) => void;
 }
 
-const RequestDropBox = ({ mode, selectedValue, width, onChangeSelect }: Props) => {
-  const [isOpen, setIsOpen] = useState(false);
-  const { data: generationData } = useSWR<IGeneration[]>('/v1/api/generation', fetcher);
+const positions = ['백엔드', '프론트엔드', '디자인', '기획'];
 
-  const generationDropRef = useRef<HTMLDivElement>(null);
+const RequestDropBox = ({
+  mode,
+  width,
+  selectedGeneration,
+  selectedPosition,
+  onChangeGeneration,
+  onChangePosition,
+}: Props) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const { data: generations } = useSWR<IGeneration[]>('/v1/api/generation', fetcher);
+
+  const dropRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
     const handleClick = (e: MouseEvent) => {
-      if (generationDropRef.current && !generationDropRef.current.contains(e.target as Node)) {
+      if (dropRef.current && !dropRef.current.contains(e.target as Node)) {
         setIsOpen(false);
       }
     };
     window.addEventListener('mousedown', handleClick);
     return () => window.removeEventListener('mousedown', handleClick);
-  }, [generationDropRef]);
+  }, [dropRef]);
+
+  const getSelectedValue = useCallback(() => {
+    if (mode === 'generation') {
+      if (selectedGeneration) {
+        return `${selectedGeneration.generationNumber}기`;
+      } else {
+        return '기수';
+      }
+    } else if (mode === 'position') {
+      if (selectedPosition) {
+        return selectedPosition;
+      } else {
+        return '포지션';
+      }
+    }
+  }, [mode, selectedGeneration, selectedPosition]);
+
+  const onClickGeneration = useCallback(
+    (generation: IGeneration) => {
+      if (onChangeGeneration) onChangeGeneration(generation);
+      setIsOpen(false);
+    },
+    [selectedGeneration],
+  );
+
+  const onClickPosition = useCallback(
+    (position: string) => {
+      if (onChangePosition) onChangePosition(position);
+      setIsOpen(false);
+    },
+    [selectedGeneration],
+  );
 
   return (
-    <SelectWrapper>
-      <SelectMenu width={width}>
-        {selectedValue ? selectedValue : mode === 'generation' ? '기수' : '포지션'}
+    <SelectWrapper ref={dropRef}>
+      <SelectMenu width={width} onClick={() => setIsOpen(!isOpen)}>
+        <p>{getSelectedValue()}</p>
+        {isOpen ? <ArrowUp /> : <ArrowDown />}
       </SelectMenu>
+      {isOpen && (
+        <SelectList>
+          <ul>
+            {mode === 'generation'
+              ? generations?.map((generation) => (
+                  <li key={generation.generationId} onClick={() => onClickGeneration(generation)}>
+                    {`${generation.generationNumber}기`}
+                  </li>
+                ))
+              : positions.map((position, index) => (
+                  <li key={index} onClick={() => onClickPosition(position)}>
+                    {position}
+                  </li>
+                ))}
+          </ul>
+        </SelectList>
+      )}
     </SelectWrapper>
   );
 };
@@ -52,7 +115,7 @@ const SelectMenu = styled.div<{ width: string }>`
   width: ${(props) => props.width};
   height: 100%;
   border: 2px solid #deead4;
-  border-radius: 12px;
+  border-radius: 10px;
   background: #fff;
 
   > p {
@@ -61,5 +124,47 @@ const SelectMenu = styled.div<{ width: string }>`
     font-size: 16px;
     font-weight: 400;
     text-transform: capitalize;
+    margin-left: 4px;
+  }
+
+  svg {
+    cursor: pointer;
+
+    path {
+      fill: #848484;
+    }
+  }
+`;
+
+const SelectList = styled.div`
+  z-index: 1;
+  position: absolute;
+  top: 36px;
+  left: 0;
+  width: 100%;
+  min-height: 36px;
+  flex-shrink: 0;
+  border: 2px solid #deead4;
+  border-radius: 10px;
+  background: #fff;
+
+  > ul {
+    display: flex;
+    flex-direction: column;
+    padding: 4px;
+    margin: 0px;
+
+    > li {
+      display: flex;
+      justify-content: center;
+      list-style: none;
+      cursor: pointer;
+      color: #5a5a5a;
+      font-family: NanumSquareRound;
+      font-size: 16px;
+      font-weight: 400;
+      text-transform: capitalize;
+      padding: 4px;
+    }
   }
 `;
---
+++ src/pages/MyPage/request/RequestContent.tsx
@@ -0,0 +1,118 @@
+import React, { useCallback, useState } from 'react';
+import { Box } from '@pages/MyPage/RoleApproveLayout';
+import { IApplyMember, IGeneration } from '@/typing/db';
+import RequestDropBox from '@components/RequestDropBox';
+import { styled } from 'styled-components';
+import ApprovePopup from '@pages/MyPage/request/RequestPopup';
+
+interface Props {
+  mode: string;
+  member: IApplyMember;
+}
+
+const RequestContent = ({ mode, member }: Props) => {
+  const [generation, setGeneration] = useState<IGeneration>();
+  const [position, setPosition] = useState('');
+  const [popupOpen, setPopupOpen] = useState(false);
+  const [popupMode, setPopupMode] = useState('');
+
+  const onChangeGeneration = useCallback(
+    (selectedGeneration: IGeneration) => {
+      setGeneration(selectedGeneration);
+    },
+    [generation],
+  );
+
+  const onChangePostion = useCallback(
+    (selectedPosition: string) => {
+      setPosition(selectedPosition);
+    },
+    [position],
+  );
+
+  const onClickApproveButton = useCallback(() => {
+    setPopupMode('approve');
+    setPopupOpen(true);
+  }, []);
+
+  const onClickRejectButton = useCallback(() => {
+    setPopupMode('reject');
+    setPopupOpen(true);
+  }, []);
+
+  const onClickReapproveButton = useCallback(() => {
+    setPopupMode('reapprove');
+    setPopupOpen(true);
+  }, []);
+
+  return (
+    <>
+      <Box>
+        <p>{member.name}</p>
+        <SettingContainer>
+          <RequestDropBox
+            mode="generation"
+            width="80px"
+            selectedGeneration={generation}
+            onChangeGeneration={onChangeGeneration}
+          />
+          <RequestDropBox
+            mode="position"
+            width="120px"
+            selectedPosition={position}
+            onChangePosition={onChangePostion}
+          />
+          {mode === 'apply' ? (
+            <>
+              <RequestButton onClick={onClickApproveButton} background="#85C88A">
+                <p>승인</p>
+              </RequestButton>
+              <RequestButton onClick={onClickRejectButton} background="#EB5353">
+                <p>거절</p>
+              </RequestButton>
+            </>
+          ) : (
+            <RequestButton onClick={onClickReapproveButton} background="#85C88A">
+              <p>재승인</p>
+            </RequestButton>
+          )}
+        </SettingContainer>
+      </Box>
+      <ApprovePopup
+        mode={popupMode}
+        member={member}
+        generation={generation}
+        position={position}
+        isOpen={popupOpen}
+        setIsOpen={setPopupOpen}
+      />
+    </>
+  );
+};
+
+export default RequestContent;
+
+const SettingContainer = styled.div`
+  display: flex;
+  gap: 8px;
+`;
+
+const RequestButton = styled.button<{ background: string }>`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 72px;
+  height: 36px;
+  border: none;
+  border-radius: 10px;
+  background: ${(props) => props.background};
+  cursor: pointer;
+
+  > p {
+    color: #fff;
+    font-family: NanumSquareRound;
+    font-size: 16px;
+    font-weight: 700;
+    text-transform: capitalize;
+  }
+`;
