841b582fd2424e65a434b0f8b1d5e06b07568269
WONYOUNG-HC <whdnjsdud551@gmail.com>
2024-03-21 00:57:43 +0900

    del: delete sort apply member file

--- src/utils/sortApplyMember.ts
+++
@@ -1,8 +0,0 @@
-import { IApplyMember } from '@/typing/db';
-
-const sortApplyMember = (members?: IApplyMember[]): IApplyMember[] | undefined => {
-  members?.sort((left, right) => left.name.localeCompare(right.name));
-  return members;
-};
-
-export default sortApplyMember;
